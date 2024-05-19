cdfba7b901420ee0790996620638c4bb9f83aa7e
leovewc <1285737503@qq.com>
2024-05-19 15:22:39 +0800

    test

---
+++ themes/shoka/scripts/random-gallery-tag.js
@@ -0,0 +1,7 @@
+// scripts/random-gallery-tag.js
+hexo.extend.tag.register('randomgallery', function (args) {
+  return `
+    <img id="random-image" alt="Random Image" style="width: 100%; height: auto;">
+    <script src="/js/random-gallery.js"></script>
+  `;
+});
